1b62314d34b7ae6d9ebb0f6a117109a12ce29d0e
aisbatu <aisyahdwi3385@student.unri.ac.id>
2024-11-27 13:44:26 +0700

    perbaikan

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.bangkit.test"
-        minSdk = 24
+        minSdk = 31
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
