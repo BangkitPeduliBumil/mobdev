66f5e50addcd235c841e2f9f82a69cc83c27475f
aisbatu <aisyahdwi3385@student.unri.ac.id>
2024-12-01 04:49:25 +0700

    newer

--- app/src/main/java/com/bangkit/pedulibumil/ui/article/ArticleAdapter.kt
+++ app/src/main/java/com/bangkit/pedulibumil/ui/article/ArticleAdapter.kt
@@ -6,7 +6,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bangkit.pedulibumil.R
 import com.bangkit.pedulibumil.databinding.ItemArticleBinding
 import com.bumptech.glide.Glide
-//test lg
+//test trs
 class ArticleAdapter(
     private val articles: List<NewsResultsItem>,
     private val onClick: (String) -> Unit // Mengirimkan link artikel
